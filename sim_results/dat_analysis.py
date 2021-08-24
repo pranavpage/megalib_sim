569f021922213b16e6ad99603d90be8ff4c0c29f
Pranav Page <pranavpage33@gmail.com>
2021-08-24 09:08:45 +0530

    Minor changes in analysis, added cubesat geometry

--- sim_results/dat_analysis.py
+++ sim_results/dat_analysis.py
@@ -2,19 +2,36 @@ import numpy as np
 import pandas as pd
 import string
 import csv
-comp_data=[i.split() for i in open("First_Second_Interaction_Coords.dat").readlines()]
-cols=[["ID", "X1", "Y1", "Z1", "X2", "Y2", "Z2"]]
-with open("comp_positions.csv", "wb") as f:
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+f_name="comp_positions_45deg_300keV.csv"
+comp_data=[i.split() for i in open("/media/pranav/page/Project_Daksha/MEGALIB_PARAM/megalib/First_Second_Interaction_Coords.dat").readlines()]
+cols=[["X1", "Y1", "Z1","E1", "X2", "Y2", "Z2", "E2"]]
+with open(f_name, "w") as f:
     writer=csv.writer(f)
     writer.writerows(cols)
     writer.writerows(comp_data)
-
-df=pd.read_csv("comp_positions.csv")
+df=pd.read_csv(f_name)
+print("Reading file")
 #First hit in Scatterer
 comp_count=0
 total_count=0
-scatterer_count=0
-abs_count=np.zeros(6, dtype=int)
+#scatterer_count=0
+#abs_count=np.zeros(6, dtype=int)
+print("Total number of Compton events : {}".format(len(df)))
+df=df[(df['Z1']==0.0) & (df['X1']<=5.0) & (df['X2']!=0)]
+theta_deg=np.degrees(np.arccos(df['Z2']/(np.sqrt(df['X2']**2+df['Y2']**2+df['Z2']**2))))
+df["theta_deg"]=theta_deg
+print(df.head())
+print("\t Events with first hit in CZT plate : {}".format(len(df)))
+df.to_csv("s_{}".format(f_name))
+plt.hist(theta_deg, bins=50)
+plt.show()
+
+        #if(row['ID']%100==0):
+            #ax.scatter3D(row['X2'], row['Y2'], row['Z2'], color='g')
+
+'''
 for index, row in df.iterrows():
     if(row['Z1']==-11.975):
         scatterer_count+=1
@@ -41,3 +58,4 @@ print("Events with first hit in Scatterer : {}".format(scatterer_count))
 print("\t Events with first hit in Scatterer and second hit in one of the Absorbers : {}".format(comp_count))
 for i in range(6):
     print("\t Second hit in Absorber {} : {}".format(i+1, abs_count[i]))
+'''
